6ca41f388eee40b54d0e1d00cdf1605f8114178a
Duncan kariuki <duncankariuki60@gmail.com>
2021-11-06 21:13:54 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'eatbit-frontend';
+export class AppComponent implements OnInit {
+  title = 'EatBit';
+  menus:any = []
+
+  constructor(private http: HttpClient) {
+   
+  }
+
+
+  getRecipies(){
+    const url ='http://localhost:3000/api/restaurant/getMenus'
+    this.http.get(url).subscribe((res)=>{
+      this.menus = res
+      console.log(this.menus)
+    })
+  }
+
+  ngOnInit(): void {
+    this.getRecipies()
+  }
+
 }
