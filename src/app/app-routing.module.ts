22a08d39ee55c4db697b407031358d61cfa4ee35
Stacy Kiriiri <wkiriiri@gmail.com>
2021-11-14 16:32:59 +0300

    add new code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { ListingComponent } from './listing/listing.component';
 import { CustomerComponent } from './customer/customer.component';
 import { AdminComponent } from './admin/admin.component';
+import { CityComponent } from './city/city.component';
+import { CuisineComponent } from './cuisine/cuisine.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'listing', component: ListingComponent },
+  { path: 'city', component: CityComponent },
+  { path: 'cuisine', component: CuisineComponent },
   {path:'customer', component: CustomerComponent},
   {path:'admin', component: AdminComponent}
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,22 @@ import { HttpClient } from "@angular/common/http";
 })
 export class AppComponent implements OnInit {
 
+  cities:any = []
+
   constructor(private http: HttpClient) {
    
   }
+
+  getCities(){
+    const url ='http://localhost:3000/api/restaurant/getCities'
+    this.http.get(url).subscribe((res)=>{
+      this.cities = res
+      console.log(this.cities)
+    })
+  }
+
   ngOnInit(): void {
-    
+    this.getCities();
   }
 
   
